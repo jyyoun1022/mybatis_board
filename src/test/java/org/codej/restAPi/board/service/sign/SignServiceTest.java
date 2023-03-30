dcd447329e6c223533c45c043f7ac9d98346b981
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-03-30 17:39:53 +0900

    토큰 검증, 토큰 발급  및 로그인관련 서비스 서비스 단위테스트

---
+++ src/test/java/org/codej/restAPi/board/service/sign/SignServiceTest.java
@@ -0,0 +1,94 @@
+package org.codej.restAPi.board.service.sign;
+
+import org.codej.restAPi.board.dto.Member;
+import org.codej.restAPi.board.dto.sign.SignInRequest;
+import org.codej.restAPi.board.dto.sign.SignInResponse;
+import org.codej.restAPi.board.dto.sign.SignUpRequest;
+import org.codej.restAPi.board.entity.Role;
+import org.codej.restAPi.board.exception.CustomException;
+import org.codej.restAPi.board.mapper.MemberMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class SignServiceTest {
+
+    @InjectMocks
+    SignService signService;
+    @Mock
+    MemberMapper memberMapper;
+    @Mock
+    PasswordEncoder passwordEncoder;
+    @Mock
+    TokenService tokenService;
+
+    @Test
+    void signUpTest(){
+        //given
+        SignUpRequest req = createSignUpRequest();
+        given(passwordEncoder.encode(any())).willReturn("password1");
+        //when
+        signService.signUp(req);
+        //then
+        verify(passwordEncoder).encode(req.getPassword());
+        verify(memberMapper).save(any());
+    }
+    @Test
+    void validateSignUpDuplicateEmailTest(){
+        //given
+        given(memberMapper.existsByEmail(anyString())).willReturn(true);
+        //when//then
+        assertThatThrownBy(() -> signService.signUp(createSignUpRequest()))
+                .isInstanceOf(CustomException.class);
+    }
+    @Test
+    void validateSignUpByDuplicateNicknameTest(){
+        //given
+        given(memberMapper.existsByNickname(anyString())).willReturn(true);
+        //when,then
+        assertThatThrownBy(() -> signService.signUp(createSignUpRequest()))
+                .isInstanceOf(CustomException.class);
+    }
+    @Test
+    void signInTest(){
+        //given
+//        given(memberMapper.findByEmail(any())).willReturn(Optional.of(createMember()));
+        given(passwordEncoder.matches(anyString(),anyString())).willReturn(true);
+        given(tokenService.createAccessToken(anyString())).willReturn("access");
+        given(tokenService.createRefreshToken(anyString())).willReturn("refresh");
+        given(memberMapper.getPassword(anyString())).willReturn("password");
+
+        //when
+        SignInResponse res = signService.signIn(new SignInRequest("email", "password"));
+        //then
+        assertThat(res.getAccessToken()).isEqualTo("access");
+        assertThat(res.getRefreshToken()).isEqualTo("refresh");
+    }
+    @Test
+    void signInExceptionByNoneMemberTest(){
+        //given
+        given(memberMapper.getPassword(anyString())).willReturn("password1");
+        //when,then
+        assertThatThrownBy(() -> signService.signIn(new SignInRequest("email","password")))
+                .isInstanceOf(CustomException.class);
+    }
+    private SignUpRequest createSignUpRequest() {
+        return new SignUpRequest("email@gmail.com", "password1", "codej", "윤재열","1996-10-22");
+    }
+
+    private Member createMember() {
+        return new Member("codej", "password", "윤재열", "email@gmail.com", Role.USER.getKey(), "1996-1022", 26);
+    }
+
+}
---
+++ src/test/java/org/codej/restAPi/board/service/sign/ToKenServiceTest.java
@@ -0,0 +1,71 @@
+package org.codej.restAPi.board.service.sign;
+
+import org.codej.restAPi.board.security.JwtHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+/**
+ * TokenService를 테스트하기 위함이지 의존관계에 있는 다른 객체들을 테스트하려는것은 아니다.
+ * @InjectMocks는 의존성을 가지고 있는 객체들을 가짜로 만들어서 주입받을 수 있다.
+ * @Mock 객체들을 가짜로 만들어서 @InjectMocks로 지정된 객체에 주입해준다.
+ * given() 메서드는 의존하는 가짜 행위가 반환해야할 데이터를 미리 준비하여 주입
+ * verify() 메서드는 가짜 객체가 수행한 행위도 검증이 가능
+ */
+@ExtendWith(MockitoExtension.class)
+public class ToKenServiceTest {
+    @InjectMocks
+    TokenService tokenService;
+    @Mock
+    JwtHandler jwtHandler;
+
+    /**
+     * @Value를 통해서 설정 파일에서 값을 읽어와야 한다.
+     * 하지만 해당값을 읽어올 수 없기에 ReflectionTestUtils를 사용하여(리플렉션) 어떠한 객체의 필드값을 주입가능
+     */
+    @BeforeEach
+    void init(){
+        ReflectionTestUtils.setField(tokenService, "accessTokenMaxAgeSeconds", 10L);
+        ReflectionTestUtils.setField(tokenService, "refreshTokenMaxAgeSeconds", 10L);
+        ReflectionTestUtils.setField(tokenService, "accessKey", "accessKey");
+        ReflectionTestUtils.setField(tokenService, "refreshKey", "refreshKey");
+    }
+
+    /**
+     *
+     */
+    @Test
+    void createAccessTokenTest(){
+        //given
+        given(jwtHandler.createToken(anyString(),anyString(),anyLong())).willReturn("access");
+        //when
+        String token = tokenService.createAccessToken("subject");
+
+        //then
+        assertThat(token).isEqualTo("access");
+        verify(jwtHandler).createToken(anyString(),anyString(),anyLong());
+
+    }
+    @Test
+    void createRefreshTokenTest() {
+        // given
+        given(jwtHandler.createToken(anyString(), anyString(), anyLong())).willReturn("refresh");
+
+        // when
+        String token = tokenService.createRefreshToken("subject");
+
+        // then
+        assertThat(token).isEqualTo("refresh");
+        verify(jwtHandler).createToken(anyString(), anyString(), anyLong());
+    }
+}
