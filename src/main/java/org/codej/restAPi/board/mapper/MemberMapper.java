dd92ef033a75fa7297c05824ab9e1fcdc3c40932
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-03-30 17:41:29 +0900

    Member 관련 Mapper

---
+++ src/main/java/org/codej/restAPi/board/mapper/MemberMapper.java
@@ -0,0 +1,30 @@
+package org.codej.restAPi.board.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.codej.restAPi.board.dto.Member;
+import org.codej.restAPi.board.dto.MemberUpdateDto;
+import org.codej.restAPi.board.dto.sign.SignUpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Mapper
+@Component
+public interface MemberMapper {
+
+    int save(SignUpRequest req);
+    int countByNickname(String nickname);
+    void withdrawMember(Long memberId);
+    String findByMemberId(int memberId);
+    int updateNickname(MemberUpdateDto dto);
+
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+    boolean existsByMemberId(Long id);
+
+    String getPassword(String email);
+    int getMemberId(String email);
+
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findById(Long id);
+}
