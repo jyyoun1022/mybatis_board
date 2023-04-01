daa3820eb67f27a9389b1090078345e5c44d4e0b
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-04-02 02:17:02 +0900

    Exception 컨트롤러

---
+++ src/main/java/org/codej/restAPi/board/controller/exception/ExceptionController.java
@@ -0,0 +1,19 @@
+package org.codej.restAPi.board.controller.exception;
+
+import org.codej.restAPi.board.exception.AccessDeniedException;
+import org.codej.restAPi.board.exception.AuthenticationEntryPointException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExceptionController {
+
+    @GetMapping("/exception/entry-point")
+    public void entryPoint(){
+        throw new AuthenticationEntryPointException();
+    }
+    @GetMapping("/exception/access-denied")
+    public void accessDenied() {
+        throw new AccessDeniedException();
+    }
+}
